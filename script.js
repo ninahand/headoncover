692ec82bf06130f4b3ff1bf70437109ed8ab44c2
ninahand <ninahand@newschool.edu>
2024-04-11 10:11:47 -0400

    first push

---
+++ script.js
@@ -0,0 +1,94 @@
+
+//found a similar function online + used ChatGPT to debug!
+function flipThroughImages() {
+    let currentIndex = 1;
+    let imageContainer = document.querySelector('.image-container');
+    let croppedImages = imageContainer.getElementsByTagName('img');
+
+    function showNextImage() {
+        croppedImages[currentIndex - 1].style.display = 'none'; 
+        currentIndex++;
+        if (currentIndex > croppedImages.length) {
+            currentIndex = 1; 
+        }
+        croppedImages[currentIndex - 1].style.display = 'block';
+    }
+    for (var i = 0; i < croppedImages.length; i++) {
+        croppedImages[i].style.display = 'none';
+    }
+
+    setInterval(showNextImage, 150); 
+}
+
+flipThroughImages();
+
+
+let allSongs = [];
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch("./data.json")
+    .then ((response)=> response.json())
+    .then((items) =>{
+        items.forEach((item) => { 
+            if(!allSongs.includes(item.title) && item.title){
+                allSongs.push(item.title);
+                console.log("allSongs", allSongs);
+            }
+
+            let songDescriptionWrapper = document.createElement('div');
+            songDescriptionWrapper.classList.add("song-description-wrapper");
+            songDescriptionWrapper.classList.add("hidden");
+
+
+            let cover = document.createElement('img');
+            cover.setAttribute('src', item.image);
+            cover.classList.add('cover');
+
+
+            let song = document.createElement('div');
+            song.innerText = item.title;
+            song.setAttribute('data-like', item.like);
+            song.classList.add('song');
+
+
+            let artist = document.createElement('div');
+            artist.innerText = item.artist;
+            artist.classList.add('artist');
+
+            let record = document.createElement('div');
+            record.innerText = item.record;
+            record.classList.add('record');
+
+            songDescriptionWrapper.appendChild(cover);
+            songDescriptionWrapper.appendChild(song);
+            songDescriptionWrapper.appendChild(artist);
+            songDescriptionWrapper.appendChild(record);
+
+
+            let indexContainer = document.querySelector('.index-container');
+            indexContainer.appendChild(songDescriptionWrapper);
+        });
+    });
+});
+
+
+let titleButton = document.querySelector(".title-index");
+let gif = document.querySelector(".image-container");
+let index = document.querySelector('.song-description-wrapper');
+
+titleButton.addEventListener("click", (e) => {
+    let songDescriptionWrappers = document.querySelectorAll('.song-description-wrapper');
+
+    if (gif.classList.contains('hidden')){
+        gif.classList.remove('hidden')
+        songDescriptionWrappers.forEach(wrapper=>{
+            wrapper.classList.add('hidden')
+        })
+    } else{
+        gif.classList.add('hidden')
+        songDescriptionWrappers.forEach(wrapper=>{
+            wrapper.classList.remove('hidden');
+        })
+    }
+
+});
